876ee911d3f785557e774cdd0871b11549ecb87c
Valery Bortnikova <valery.ivlieva@gmail.com>
2021-04-02 13:33:05 +0300

    sign in page object

---
+++ page_objects/sign_in.py
@@ -0,0 +1,35 @@
+class SignInPage:
+    def __init__(self, page, link):
+        self.page = page
+        self.link = link
+
+    def open(self):
+        self.page.goto(self.link)
+
+    def fill_login(self, login):
+        self.page.fill('input[name="login"]', login)
+
+    def fill_password(self, password):
+        self.page.fill('input[name="password"]', password)
+
+    def sign_in(self):
+        with self.page.expect_navigation(timeout=5000):
+            self.page.click("text='Войти'")
+
+    def sign_in_with_incorrect_data(self):
+        self.page.click("text='Войти'")
+
+    def assert_login_successful(self):
+        assert "application" in self.page.url, 'Authentication failed'
+
+    def assert_login_unsuccessful(self):
+        popup_message = self.page.inner_text('div[role="status"] div div', timeout=3000)
+        assert popup_message == "Неудачная попытка авторизоваться", 'There is no popup-message'
+
+    def assert_sign_in_by_sms_button_visible(self):
+        visible = self.page.is_visible("text='Войти по SMS'")
+        assert visible, 'There is no new button'
+
+    def assert_sign_in_by_sms_button_invisible(self):
+        visible = self.page.is_visible("text='Войти по SMS'")
+        assert not visible, 'There is wrong button'
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,44 +1,70 @@
 import pytest
 
+from page_objects.sign_in import SignInPage
+
+link = "https://dev.lk.tr-line.ru/sign-in"
 
 testdata_correct = [
     ["gipoxor556@asfalio.com", "5Vx8LvGrno8p"],
-    ["+71111111112", "5Vx8LvGrno8p"]
+    ["+71111111112", "5Vx8LvGrno8p"],
 ]
-
 
 testdata_incorrect = [
     ["abc", "0"],
     ["", "5Vx8LvGrno8p"],
     ["gipoxor556@asfalio.com", ""],
     ["+71111111112", ""],
-    ["", ""]
+    ["", ""],
+]
+
+testdata_phone_number = [
+    '+71111111112',
+    '89876543210',
+    '0',
+    '9',
+    '+',
+    '(',
+]
+
+testdata_incorrect_phone_number = [
+    '@',
+    '.',
 ]
 
 
 @pytest.mark.parametrize("login, password", testdata_correct)
 def test_auth_correct_data(page, login, password):
-    page.goto("https://dev.lk.tr-line.ru/sign-in")
-    page.fill('input[name="login"]', login)
-    page.fill('input[name="password"]', password)
-    with page.expect_navigation(timeout=5000):
-        page.click("text='Войти'")
-    assert "application" in page.url
+    sign_in_page = SignInPage(page, link)
+    sign_in_page.open()
+    sign_in_page.fill_login(login)
+    sign_in_page.fill_password(password)
+    sign_in_page.sign_in()
+    sign_in_page.assert_login_successful()
 
 
 @pytest.mark.parametrize("login, password", testdata_incorrect)
 def test_auth_incorrect_data(page, login, password):
-    page.goto("https://dev.lk.tr-line.ru/sign-in")
-    page.fill('input[name="login"]', login)
-    page.fill('input[name="password"]', password)
-    page.click("text='Войти'")
-    popup_message = page.inner_text('div[role="status"] div div', timeout=3000)
-    assert popup_message == "Неудачная попытка авторизоваться"
+    sign_in_page = SignInPage(page, link)
+    sign_in_page.open()
+    sign_in_page.fill_login(login)
+    sign_in_page.fill_password(password)
+    sign_in_page.sign_in_with_incorrect_data()
+    sign_in_page.assert_login_unsuccessful()
 
 
-def test_auth_by_phone_number(page):
-    page.goto("https://dev.lk.tr-line.ru/sign-in")
-    page.fill('input[name="login"]', "+71111111112")
-    visible = page.is_visible("text='Войти по SMS'")
-    assert visible
+@pytest.mark.parametrize("login", testdata_phone_number)
+def test_auth_by_phone_number(page, login):
+    sign_in_page = SignInPage(page, link)
+    sign_in_page.open()
+    sign_in_page.fill_login(login)
+    sign_in_page.assert_sign_in_by_sms_button_visible()
+
+
+@pytest.mark.parametrize("login", testdata_incorrect_phone_number)
+def test_sign_in_by_sms_button_invisible(page, login):
+    sign_in_page = SignInPage(page, link)
+    sign_in_page.open()
+    sign_in_page.fill_login(login)
+    sign_in_page.assert_sign_in_by_sms_button_invisible()
+
 

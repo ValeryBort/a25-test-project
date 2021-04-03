fabf00e7579a455b20e8252f691793df7dda4196
Valery Bortnikova <valery.ivlieva@gmail.com>
2021-04-03 21:50:08 +0300

    add test cases numbers

--- tests/test_restore_password.py
+++ tests/test_restore_password.py
@@ -14,15 +14,11 @@ email_incorrect = [
 ]
 
 
-def test_restore_password_with_empty_field(page):
-    res_pass_page = RestorePasswordPage(page, link)
-    res_pass_page.open()
-    res_pass_page.click_reset_password()
-    res_pass_page.assert_popup_message_unsuccessful_restore()
-
-
 @pytest.mark.parametrize("email", email_not_registered)
 def test_restore_password_with_not_registered_email(page, email):
+    """
+    Тест-кейс 3.2
+    """
     res_pass_page = RestorePasswordPage(page, link)
     res_pass_page.open()
     res_pass_page.fill_email(email)
@@ -30,8 +26,21 @@ def test_restore_password_with_not_registered_email(page, email):
     res_pass_page.assert_popup_message_unsuccessful_restore()
 
 
+def test_restore_password_with_empty_field(page):
+    """
+    Тест-кейс 3.3
+    """
+    res_pass_page = RestorePasswordPage(page, link)
+    res_pass_page.open()
+    res_pass_page.click_reset_password()
+    res_pass_page.assert_popup_message_unsuccessful_restore()
+
+
 @pytest.mark.parametrize("email", email_incorrect)
 def test_restore_password_with_incorrect_email(page, email):
+    """
+    Тест-кейс 3.4
+    """
     res_pass_page = RestorePasswordPage(page, link)
     res_pass_page.open()
     res_pass_page.fill_email(email)
